296e679606f5e7230efdfa89669a85d4937fe9cf
mkumar87 <mkumar87@gmail.com>
2015-08-20 15:06:54 +0530

    Create LocalTesting.java
    Creating the new file

---
+++ LocalTesting.java
@@ -0,0 +1,27 @@
+public class LocalTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+		String ordVer = "01";
+		String orderNumber = "56658999";
+		String orderId = (orderNumber == null) ? orderNumber : orderNumber.trim();
+		
+		if(ordVer == null || ordVer.length() <=0)
+		{
+			if(orderId.trim().length() == 9){
+				orderNumber = orderId.substring(0,orderId.length()-1);
+				ordVer = orderId.substring(orderId.length()-1);
+			}else{
+				orderNumber = orderId.substring(0);
+			}
+		}
+		
+		System.out.println("Order Number is " + orderNumber);
+		System.out.println("Order Version is" + ordVer);
+
+	}
+}
